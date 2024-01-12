e2f5bb2e68241dbe357044daaa2223ab02c160f3
RelancioBorgesDev <relancio.dev@gmail.com>
2024-01-12 14:55:44 -0300

    feat: estrutura inicial

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,42 @@
+export const defaultTheme = {
+  colors: {
+    "yellow-dark": "#c47f17",
+    yellow: "#dbac2c",
+    "yellow-light": "#f1e9c9",
+    "purple-dark": "#4b2995",
+    purple: "#8047f8",
+    "purple-light": "#ebe5f9",
+    "base-title": "#272221",
+    "base-subtitle": "#403937",
+    "base-text": "#574f4d",
+    "base-label": "#8d8686",
+    "base-hover": "#d7d5d5",
+    "base-button": "#e6e5e5",
+    "base-input": "#ededed",
+    "base-card": "#f3f2f2",
+    background: "#fafafa",
+    white: "#FFFFFF",
+  },
+  fontFamily: {
+    "baloo-2": "'Baloo 2', sans-serif",
+    roboto: "'Roboto', sans-serif",
+  },
+  fontSize: {
+    "baloo-XL": "3rem", //48px
+    "baloo-L": "2rem", //32px
+    "baloo-M": "1.5rem", //24px
+    "baloo-S": "1.25rem", //20px
+    "baloo-XS": "1.125rem", //18px
+    "roboto-L": "1.25rem", //20px
+    "roboto-M": "1rem", //16px
+    "roboto-S": "0.875rem", //14px
+    "roboto-XS": "0.75rem", //12px
+    tag: "0.625rem",
+    "button-G": "0.875rem",
+    "button-M": "0.75rem",
+  },
+  lineHeight: {
+    normal: "130%",
+    high: "160%",
+  },
+};
